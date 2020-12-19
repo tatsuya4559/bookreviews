3921d7d5279d14ce48c49ccdc561c1379157e92c
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-12-19 09:35:29 +0900

    考えた過程をメモしておく

--- handlers.go
+++ handlers.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+// HandlerFuncに値を渡すには
+// context, グローバル変数, ハンドラーをメソッドにして構造体フィールドに持たせる
+// が考えられる。
+// contextはライフタイム管理のためのものなのでNG
+// 構造体フィールドにするとその構造体が巨大になるし、どのハンドラが何に依存しているのかわかりにくい
+// というわけでDIコンテナだけはグローバル変数に持たせて妥協してハンドラの中でInvokeを使ってみた
+// コンテナだけならグローバルでもいいかな
 func booksIndex(w http.ResponseWriter, r *http.Request) {
 	container.Invoke(func(repo BookRepository) {
 		// ここでサービス層がDIされてきたりするけど、
